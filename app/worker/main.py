53b413a07257709179d1ec8b236dfb41d5deb390
Lukas Zimmermann <lukas@Lukass-MacBook-Air.local>
2025-05-21 20:12:28 +0200

    update main

--- app/worker/main.py
+++ app/worker/main.py
@@ -1,50 +1,36 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
 import time
-
+from pytubefix.exceptions import BotDetection, AgeRestrictedError
 from app.worker.youtube.channel import YoutubeChannel
 from app.worker.youtube.downloader import YoutubeDownloader
-
-# class Scanner():
-#     def __init__(self):
-#         self.is_running = False
-
-#     def start_scan(self):
-#         if not self.is_running:
-#             try:
-#                 self.is_running = True
-#                 y = YoutubeDownloader()
-#                 y.download("https://www.youtube.com/watch?v=AeJ9q45PfD0")
-#             except Exception as e:
-#                 pass
-#             finally: 
-#                 self.is_running = False
-
-# scanner = Scanner()
-
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(
-#     scanner.start_scan,
-#     trigger='cron',
-#     # minute='*',
-#     hour=2,
-#     minute=0,
-#     next_run_time=datetime.now())
-
-# scheduler.start()
-
-# try:
-#     while True:
-#         time.sleep(0.1)
-# except (KeyboardInterrupt, SystemExit):
-#     scheduler.shutdown()
 
 print("Start")
 channel_handler = YoutubeChannel(enable_proxy=True)
 
 urls = channel_handler.get_new_video_urls()
 
-for url in urls[:20]:
+time_interval = 0
+start_time = 0
+
+for url in urls:
+    start_time = time.time()
     print(url)
-    youtube_handler = YoutubeDownloader(enable_proxy=True)
-    youtube_handler.download(url)
+    try:
+        youtube_handler = YoutubeDownloader(enable_proxy=False)
+        youtube_handler.download(url)
+
+
+        time_to_sleep = time_interval-(time.time()-start_time)
+
+        if time_to_sleep > 1:
+            time.sleep(time_to_sleep)
+    except BotDetection as e:
+        wait_time = 120*60
+        wait_step = 60
+        for i in range(int(wait_time/wait_step)):
+            print(f"Bot detection. Next try in {(wait_time/wait_step-i):.0f}min")
+            time.sleep(wait_step)
+
+    except AgeRestrictedError as e:
+        print("Too young...")
+    except Exception as e:
+        print(e)
